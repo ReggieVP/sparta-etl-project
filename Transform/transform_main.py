60ba5be5faa6b94a1200adbcc328bee61a0f3673
Aymen Shallal <aymenwork2020@gmail.com>
2024-01-09 16:55:20 +0000

    Academy csv,  second tranform commit

--- Transform/transform_main.py
+++ Transform/transform_main.py
@@ -8,33 +8,34 @@ from Extract.extract_main import Extract
 
 class TransformCSV(Extract):
     def __init__(self):
-        self.meet = True
+        super().__init__()
 
     def transform_business_csvs(self):
-        instance1 = Extract()
-        business_df_transform = instance1.calling_bucket_business()
+        business_df_transform = self.calling_bucket_business()
         business_df_transform = business_df_transform.fillna(0)
         business_df_transform['Date'] = business_df_transform['Date'].apply(lambda x: x[3:])
         business_df_transform['Date'] = pd.to_datetime(business_df_transform['Date'])
-        print(type(business_df_transform['Date'][5]))
+        business_df_transform = business_df_transform.reset_index()
+        business_df_transform = business_df_transform.drop(columns=['index'])
 
         return business_df_transform
     def transform_engineering_csvs(self):
-        instance1 = Extract()
-        engineering_df_transform = instance1.calling_bucket_engineering()
+        engineering_df_transform = self.calling_bucket_engineering()
         engineering_df_transform = engineering_df_transform.fillna(0)
         engineering_df_transform['Date'] = engineering_df_transform['Date'].apply(lambda x: x[3:])
         engineering_df_transform['Date'] = pd.to_datetime(engineering_df_transform['Date'])
-        print(type(engineering_df_transform['Date'][5]))
+        engineering_df_transform = engineering_df_transform.reset_index()
+        engineering_df_transform = engineering_df_transform.drop(columns=['index'])
 
         return engineering_df_transform
     def transform_data_csvs(self):
-        instance1 = Extract()
-        data_df_transform = instance1.calling_bucket_data()
+
+        data_df_transform = self.calling_bucket_data()
         data_df_transform = data_df_transform.fillna(0)
         data_df_transform['Date'] = data_df_transform['Date'].apply(lambda x: x[3:])
         data_df_transform['Date'] = pd.to_datetime(data_df_transform['Date'])
-        print(type(data_df_transform['Date'][5]))
+        data_df_transform = data_df_transform.reset_index()
+        data_df_transform = data_df_transform.drop(columns=['index'])
 
         return data_df_transform
 
@@ -49,5 +50,8 @@ pd.set_option('display.width', None)
 
 instance2 = TransformCSV()
 
+print(instance2.transform_engineering_csvs())
 print(instance2.transform_data_csvs())
+print(instance2.transform_business_csvs())
+
 
