5164a607ddf9c01748850aa72ca44bec6272fab0
Jesse Rolfe <jesse.v.rolfe@gmail.com>
2024-01-11 15:23:26 +0000

    IDs added to merged

--- Transform/normalise.py
+++ Transform/normalise.py
@@ -1,6 +1,6 @@
 from transform_main import Transform
 import pandas as pd
-
+import numpy as np
 
 class Normalise(Transform):
 
@@ -21,6 +21,42 @@ class Normalise(Transform):
             merged_df = pd.merge(merged_df, df, on=['Forename', 'Lastname'], how='outer')
 
         merged_df.insert(0, "Student_ID", range(1, 1 + len(merged_df)))
+
+        merged_df = merged_df.sort_values('Uni')
+        merged_df['University_ID'] = (merged_df.groupby(['Uni']).cumcount()==0).astype(int)
+        merged_df['University_ID'] = merged_df['University_ID'].cumsum()
+
+        merged_df = merged_df.sort_values('Degree')
+        merged_df['Grade_ID'] = (merged_df.groupby(['Degree']).cumcount() == 0).astype(int)
+        merged_df['Grade_ID'] = merged_df['Grade_ID'].cumsum()
+
+        merged_df = merged_df.sort_values('Postcode')
+        merged_df['Postcode_ID'] = (merged_df.groupby(['Postcode']).cumcount() == 0).astype(int)
+        merged_df['Postcode_ID'] = merged_df['Postcode_ID'].cumsum()
+
+        merged_df = merged_df.sort_values('Gender')
+        merged_df['Gender_ID'] = (merged_df.groupby(['Gender']).cumcount() == 0).astype(int)
+        merged_df['Gender_ID'] = merged_df['Gender_ID'].cumsum()
+
+        merged_df = merged_df.sort_values('City')
+        merged_df['City_ID'] = (merged_df.groupby(['City']).cumcount() == 0).astype(int)
+        merged_df['City_ID'] = merged_df['City_ID'].cumsum()
+
+        merged_df = merged_df.sort_values('Address')
+        merged_df['Address_ID'] = (merged_df.groupby(['Address']).cumcount() == 0).astype(int)
+        merged_df['Address_ID'] = merged_df['Address_ID'].cumsum()
+
+        merged_df = merged_df.sort_values('Course')
+        merged_df['Course_ID'] = (merged_df.groupby(['Course']).cumcount() == 0).astype(int)
+        merged_df['Course_ID'] = merged_df['Course_ID'].cumsum()
+
+        merged_df = merged_df.sort_values(['Talent_Forename', 'Talent_Lastname'])
+        merged_df['Talent_Team_ID'] = (merged_df.groupby(['Talent_Forename', 'Talent_Lastname']).cumcount() == 0).astype(int)
+        merged_df['Talent_Team_ID'] = merged_df['Talent_Team_ID'].cumsum()
+
+        merged_df = merged_df.sort_values(['Trainer_Forename', 'Trainer_Lastname'])
+        merged_df['Trainer_ID'] = (merged_df.groupby(['Trainer_Forename', 'Trainer_Lastname']).cumcount() == 0).astype(int)
+        merged_df['Trainer_ID'] = merged_df['Trainer_ID'].cumsum()
 
         return merged_df
 
@@ -77,6 +113,26 @@ class Normalise(Transform):
                     student_strength_df.loc[len(student_strength_df.index)] = [row, strength_df["Strengths_ID"][
                         strength_df.index[strength_df["Strengths"] == word][0]]]
         return student_strength_df
+
+    def tech_score_table(self):
+        merged_df = self.merge_dataframes()
+        selected_columns = ['C#', 'Java', 'R', 'Javascript', 'Python', 'C++', 'Ruby', 'SPSS', 'PHP']
+        cols = {'Language_ID': [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                'Language': merged_df[selected_columns].columns.tolist()}
+
+        language_df = pd.DataFrame(data=cols)
+        #print(language_df)
+
+
+        tech_score_df = pd.DataFrame(columns=["Student_Id", "Langauge_ID", "Score"])
+        for row in merged_df["Student_ID"]:
+            for columns in language_df["Language"]:
+                if not np.isnan(merged_df[columns][row - 1]):
+                    tech_score_df.loc[len(tech_score_df.index)] = [row, language_df["Language_ID"]\
+                        [language_df.index[language_df["Language"] == columns][0]], merged_df[columns][row - 1]]
+
+        tech_score_df.astype(int)
+
 
 
 test = Normalise()
